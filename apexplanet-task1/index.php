a0c5ff96f6b8a2398e7d2815da11912a40ba26f1
kushagra53 <kushagrapandey096@gmail.com>
2026-07-06 00:11:45 +0530

    code correction

--- apexplanet-task1/index.php
+++ apexplanet-task1/index.php
@@ -1,14 +1,4 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
-<?php
-echo "Task 1 completed";
+echo "<h1>Task 1 Completed</h1>";
+echo "<p>PHP is working successfully.</p>";
 ?>
